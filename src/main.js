01cf00ca0e30e07a2a92813bbca535cebe5a25fd
JonathanBoutros <boutrosjo123@gmail.com>
2023-09-09 23:09:15 -0400

    website style

--- src/main.js
+++ src/main.js
@@ -3,8 +3,18 @@ import "./main.css"
 const Main = () => {
     return(
         <div className="container">
-            <div className="messageContent">
-                
+            <div className="content">
+                <div className="messageContent">
+                    <p></p>
+                </div>
+            </div>
+            <div className="inputContent">
+                <input 
+                    type="text" 
+                    className="input" 
+                    placeholder="Write your Quotes" 
+                    />
+                <button className="btn">Send</button>
             </div>
         </div>
     );    
